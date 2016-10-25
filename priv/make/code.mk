912decd7cc6cc0ed5c37a67da7595fc90cb4239b
Duncan McGreggor <oubiwann@gmail.com>
2016-10-24 23:46:14 -0500

    Added push and publish targets.

--- priv/make/code.mk
+++ priv/make/code.mk
@@ -19,3 +19,16 @@ clean:
 
 clean-all: clean
 	@rebar3 as dev lfe clean
+
+push:
+>-git push github master
+>-git push gitlab master
+
+push-tags:
+>-git push github --tags
+>-git push gitlab --tags
+
+push-all: push push-tags
+
+publish:
+>-rebar3 as hexpm hex publish
